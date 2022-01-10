9e7263bc22dcafc30dd980f6df4991d8cb777205
경태윤 <xodbsrud320@gmail.com>
2022-01-10 22:50:25 +0900

    util

--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -1,9 +1,8 @@
-package com.javaex.servlet;
+package com.javaex.controller;
 
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javaex.dao.GuestbookDao;
+import com.javaex.util.WebUtil;
 import com.javaex.vo.GuestbookVo;
 
 @WebServlet("/gbc")
@@ -21,39 +21,26 @@ public class GuestbookController extends HttpServlet {
 			throws ServletException, IOException {
 		System.out.println("GuestbookController");
 
-		GuestbookDao guestbookDao = new GuestbookDao();
-		List<GuestbookVo> guestList = guestbookDao.getList();
-
 		String action = request.getParameter("action");
 
-		if ("addlist".equals(action)) {
-			System.out.println("action=addlist");
-
-			request.setAttribute("gList", guestList);
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/addList.jsp");
-			rd.forward(request, response);
-
-		} else if ("add".equals(action)) {
+		if ("add".equals(action)) {
 			System.out.println("action=add");
 
 			String name = request.getParameter("name");
 			String password = request.getParameter("password");
 			String content = request.getParameter("content");
 
+			GuestbookDao dao = new GuestbookDao();
 			GuestbookVo vo = new GuestbookVo(name, password, content);
-			int count = guestbookDao.insert(vo);
+			int count = dao.insert(vo);
 
 			System.out.println(count + "건 등록되었습니다");
-			response.sendRedirect("/guestbook2/gbc?action=addlist");
+			WebUtil.redirect(request, response, "/guestbook2/gbc?action=addlist");
 
 		} else if ("deleteform".equals(action)) {
 			System.out.println("action=deleteform");
 
-			String stringNo = request.getParameter("no");
-			request.setAttribute("stringNo", stringNo);
-
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/deleteForm.jsp");
-			rd.forward(request, response);
+			WebUtil.forward(request, response, "/WEB-INF/deleteForm.jsp");
 
 		} else if ("delete".equals(action)) {
 			System.out.println("action=delete");
@@ -64,13 +51,20 @@ public class GuestbookController extends HttpServlet {
 			GuestbookVo vo = new GuestbookVo();
 			vo.setNo(no);
 			vo.setPassword(password);
-			int count = guestbookDao.delete(vo);
+			
+			GuestbookDao dao = new GuestbookDao();
+			int count = dao.delete(vo);
 
 			System.out.println(count + "건 삭제되었습니다.");
-			response.sendRedirect("/guestbook2/gbc?action=addlist");
+			WebUtil.redirect(request, response, "/guestbook2/gbc?action=addlist");
 
 		} else {
-			System.out.println("파라미터 없음");
+			GuestbookDao dao = new GuestbookDao();
+			List<GuestbookVo> guestList = dao.getList();
+			
+			request.setAttribute("gList", guestList);
+			
+			WebUtil.forward(request, response, "/WEB-INF/addList.jsp");
 		}
 
 	}
